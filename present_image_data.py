bde34f462332b3bb8a89db80200c69bc2ea77683
zpyuan6 <zpyuan6@163.com>
2023-04-05 16:55:15 +0100

    init

--- present_image_data.py
+++ present_image_data.py
@@ -1,18 +1,20 @@
 from skimage import io
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == "__main__":
 
-
-    tif = io.imread("F:\\Hyperspecial\\Aerial_UAV_Photos\\green.rgb.tif")
+    image_path = "F:\\Hyperspecial\\pear\\04_11_21\\Aerial_UAV_Photos\\nir.rgb.tif"
+    tif = io.imread(image_path)
     print(tif.shape)
-    print(type(tif))
 
-    # print(tif[:,:,1])
+    # if len(tif.shape)>2:
+    #     for i in range(tif.shape[2]):
+    #         plt.subplot(1,tif.shape[2],i+1)
+    #         plt.imshow(tif[:,:,i])
 
-    plt.subplot(1,2,1)
-    plt.imshow(tif[:,:,1],cmap="gray")
-    plt.subplot(1,2,2)
-    plt.imshow(tif[:,:,2],cmap="gray")
-
+    #     plt.show()
+    # else:
+    # plt.imshow(tif, cmap="gray")
+    plt.imshow(tif)
     plt.show()
